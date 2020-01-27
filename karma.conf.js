48c7e3ce24f4adce1bc6411fece4084d7a0ead3f
xobotyi <xog3@yandex.ru>
2020-01-27 15:41:02 +0300

    fix: add root files to the linting process; feat: add more directories to gitignore; chore: update lint configs;

--- karma.conf.js
+++ karma.conf.js
@@ -1,33 +1,28 @@
-module.exports = (cfg) => {
-  cfg.set(
-    {
-      browsers: ["ChromeHeadless", "Firefox"],
-      singleRun: true,
-      frameworks: ["jasmine", "karma-typescript"],
-      reporters: ["progress", "karma-typescript"],
-      preprocessors: {
-        "**/*.ts": "karma-typescript",
+module.exports = cfg => {
+  cfg.set({
+    browsers: ['ChromeHeadless', 'Firefox'],
+    singleRun: true,
+    frameworks: ['jasmine', 'karma-typescript'],
+    reporters: ['progress', 'karma-typescript'],
+    preprocessors: {
+      '**/*.ts': 'karma-typescript',
+    },
+    files: ['src/**/*.ts', 'tests/**/*.test.ts'],
+    karmaTypescriptConfig: {
+      coverageOptions: {
+        exclude: /(node_modules|test.ts$)/i,
       },
-      files: [
-        "src/**/*.ts",
-        "tests/**/*.test.ts",
-      ],
-      karmaTypescriptConfig: {
-        coverageOptions: {
-          exclude: /(node_modules|test.ts$)/i,
+      reports: {
+        lcovonly: {
+          directory: 'coverage',
+          subdirectory: () => '',
+          filename: 'lcov.info',
         },
-        reports: {
-          lcovonly: {
-            directory: "coverage",
-            subdirectory: () => "",
-            filename: "lcov.info",
-          },
-          html: {
-            directory: "coverage",
-            subdirectory: () => "",
-          },
+        html: {
+          directory: 'coverage',
+          subdirectory: () => '',
         },
       },
     },
-  );
+  });
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,57 +1,57 @@
-import ts2 from "rollup-plugin-typescript2";
-import pkg from "./package.json";
+import ts2 from 'rollup-plugin-typescript2';
+import pkg from './package.json';
 
 const input = './src/index.ts';
 
 export default [
-    {
-        input,
-        output: [
-            {
-                file: pkg.main,
-                format: "cjs",
-                exports: "named",
-            },
-            {
-                file: pkg.module,
-                format: "esm",
-                exports: "named",
-            },
-            {
-                file: 'dist/index.min.js',
-                format: "umd",
-                exports: "named",
-                name: "xobotyi",
-                extend: true,
-            },
-        ],
-        plugins: [
-            ts2({
-                    clean: true,
-                    tsconfigOverride: {
-                        compilerOptions: {
-                            target: "es5",
-                        },
-                    },
-                }),
-        ],
-    },
-    {
-        input,
-        output: {
-            file: pkg.esnext,
-            format: "esm",
+  {
+    input,
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        exports: 'named',
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+        exports: 'named',
+      },
+      {
+        file: 'dist/index.min.js',
+        format: 'umd',
+        exports: 'named',
+        name: 'xobotyi',
+        extend: true,
+      },
+    ],
+    plugins: [
+      ts2({
+        clean: true,
+        tsconfigOverride: {
+          compilerOptions: {
+            target: 'es5',
+          },
         },
-        plugins: [
-            ts2({
-                    clean: true,
-                    tsconfigOverride: {
-                        compilerOptions: {
-                            declaration: true,
-                            declarationDir: 'dist',
-                        },
-                    },
-                }),
-        ],
+      }),
+    ],
+  },
+  {
+    input,
+    output: {
+      file: pkg.esnext,
+      format: 'esm',
     },
+    plugins: [
+      ts2({
+        clean: true,
+        tsconfigOverride: {
+          compilerOptions: {
+            declaration: true,
+            declarationDir: 'dist',
+          },
+        },
+      }),
+    ],
+  },
 ];
